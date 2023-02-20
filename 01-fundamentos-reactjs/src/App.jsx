f8eef6ec692efa65b6e92a105c3cfc95c6b75714
Elias Fonseca Martins <eliasmartinsdev8@gmail.com>
2023-02-20 19:03:57 -0300

    side bard

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,15 +1,22 @@
 import { Header } from "./components/Header";
 import {Post} from "./Post";
-import './styles.css';
+import styles from './App.module.css'
+import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
     <Header/>
-      <Post 
+    <div className={styles.wrapper} >
+    <Sidebar/>
+    <main>
+    <Post 
         author='joao'
         content = 'E Legal e.e'
       />
+    </main>
+    </div>
     </div>
   );
 }
--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import styles from "./Header.module.css";
+import igniteLogo from '../assets/fire.svg.svg';
 export function Header() {
   return (
     <header className={styles.header}>
+     <img src={igniteLogo} alt="logo iginit"/>
       <strong>Ignite Feed</strong>
     </header>
   );
